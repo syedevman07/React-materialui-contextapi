d46bb98743f6bcabefa63852b038b778b07e8cae
syed <ssinezdev07@gmail.com>
2020-03-16 05:09:10 -0700

    add backend app and auth usermodel setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend'
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+AUTH_USER_MODEL='backend.User'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
