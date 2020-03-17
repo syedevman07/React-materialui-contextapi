28f6a96b459454a08eeb68fb13006e469c4c2338
syed <ssinezdev07@gmail.com>
2020-03-17 12:40:08 -0700

    implment serializers for category and subcategory

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import routers, serializers, viewsets
-from backend.models import MyUser, Category
-
+from backend.models import MyUser, Category, SubCategory
+from django.shortcuts import get_object_or_404
 
 class CategorySerializer(serializers.ModelSerializer):
   class Meta:
     model = Category
     fields = ['id', 'name']
+
+class SubCategoryRetrieveSerializer(serializers.ModelSerializer):
+  category = CategorySerializer(many=False)
+
+  class Meta:
+    model = SubCategory
+    fields = ['id', 'name', 'category']
+
+class SubCategoryUpdateSerializer(serializers.ModelSerializer):
+  category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), required=False)
+
+  class Meta:
+    model = SubCategory
+    fields = ['id', 'name', 'category']
