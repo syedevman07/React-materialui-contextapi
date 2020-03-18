5e60c807d2bac229fb6cec657ef8cb28d97a5c6b
syed <ssinezdev07@gmail.com>
2020-03-18 07:02:26 -0700

    add pagination

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -141,7 +141,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
 
 # Internationalization
