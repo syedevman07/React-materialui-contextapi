8cff42bc36f3e25ca0992e88844bb929657fe900
syed <ssinezdev07@gmail.com>
2020-03-18 11:07:41 -0700

    fix axios issue

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,13 +3,11 @@ import axios from 'axios';
 const Axios = axios.create({
   baseURL: (() => {
     let protocol = 'https://';
-    if (process.env.NODE_ENV === 'local') {
+    if (process.env.NODE_ENV === 'development') {
       protocol = 'http://';
+      return `http://localhost:8000/api`;
     }
-    if (typeof window !== 'undefined') {
-      return `${protocol}${window.location.host}/api`;
-    }
-    return `http://localhost:8000/api`;
+    return `${protocol}${window.location.host}/api`;
   })(),
 });
 
