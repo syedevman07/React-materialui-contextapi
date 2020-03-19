3dd7e0ce1e5362c05da8ce61de6237da40a25242
syed <ssinezdev07@gmail.com>
2020-03-19 07:25:00 -0700

    implemenet search control

--- frontend/src/components/users/control/search-control.js
+++ frontend/src/components/users/control/search-control.js
@@ -2,9 +2,19 @@ import React, { useState } from 'react';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
 import Search from '@material-ui/icons/Search';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { useUser } from '../../../context/user';
 
+const useStyles = makeStyles({
+  root: {
+    marginTop: '12px',
+    marginLeft: '15px',
+  },
+});
+
 const UserControl = () => {
+  const classes = useStyles();
   const { methods: { getUsers }, data: { params } } = useUser();
   const [search, setSearch] = useState(params.search || "");
   const handleKeyDown = e => {
@@ -13,11 +23,13 @@ const UserControl = () => {
       getUsers({...params, search })
     }
   };
+
   const handleChange = (e) => setSearch(e.target.value);
   return (
     <TextField
         id="input-with-icon-textfield"
         label="Search Users"
+        className={classes.root}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
         value={search}
