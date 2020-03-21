41c726381cfa334d887c1df569ec12bfb088c698
syed <ssinezdev07@gmail.com>
2020-03-21 07:36:29 -0700

    update the settings for token lifetime

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -115,7 +115,7 @@ else:
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes = 5 * 60 * 24),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes= 1 * 60 * 24),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes = 1 * 60 * 24),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 
