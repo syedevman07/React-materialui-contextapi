d82a8452bcd0ada62f5beb7db313367cadaee6d2
syed <ssinezdev07@gmail.com>
2020-03-25 03:57:56 -0700

    use dialog for creating category

--- frontend/src/components/categories/create.js
+++ frontend/src/components/categories/create.js
@@ -1,20 +1,14 @@
 import React, { useState } from 'react';
-import { Paper, Button, TextField } from '@material-ui/core';
+import { Paper, Button, TextField, DialogContent, Dialog, DialogTitle, DialogActions } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useCategory } from '../../context/category';
 
 const useStyles = makeStyles({
   root: {
     width: 500,
-    marginTop: 40,
-    marginBottom: 40,
-    margin: 'auto',
-    display: 'flex',
-    justifyContent: 'center',
-    padding: 20,
   },
   name: {
-    marginRight: 30
+
   },
   addButton: {
     marginRight: 30
@@ -27,16 +21,22 @@ export const CategoryCreate = ({ open, handleClose}) => {
   const handleNameChange = (e) => setName(e.target.value);
   const handleCreateCategory = () => {
     createCategory(name);
+    handleClose();
   }
-  if(!open) {
-    return null;
-  }
+
   return (
-    <Paper className={classes.root}>
-      <TextField className={classes.name} type="text" value={name} onChange={handleNameChange} placeholder="Category Name"/>
-      <Button color="primary" variant="contained" className={classes.addButton} onClick={handleCreateCategory}>Create</Button>
-      <Button color="secondary" variant="contained" onClick={handleClose}>Cancel</Button>
-    </Paper>
+    <Dialog open={open}>
+      <DialogTitle id="form-dialog-title" className={classes.root}>
+        Create Category
+      </DialogTitle>
+      <DialogContent>
+        <TextField className={classes.name} fullWidth type="text" value={name} onChange={handleNameChange} placeholder="Category Name"/>
+      </DialogContent>
+      <DialogActions>
+        <Button color="primary" variant="contained" className={classes.addButton} onClick={handleCreateCategory}>Create</Button>
+        <Button color="secondary" variant="contained" onClick={handleClose}>Cancel</Button>
+      </DialogActions>
+    </Dialog>
   )
 };
 
